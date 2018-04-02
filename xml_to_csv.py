c069930584eda01520e0a4e104166fc3d742f2ca
Sean Benner <sean4b@gmail.com>
2018-04-01 20:44:58 -0700

    Updated path that csv files are saved to

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -35,7 +35,7 @@ def main():
     for directory in ['train', 'test']:
         image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
+        xml_df.to_csv('{}_labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
 
 
